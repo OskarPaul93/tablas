985ef299cd8ee988928c548aaea98b1519a462af
OskarPaul93 <paul_1993oscar@hotmail.com>
2026-04-26 13:10:30 -0500

    Ejemplo tabla 5

--- multiplicar.js
+++ multiplicar.js
@@ -1,9 +1,12 @@
 function generarTabla(){
-    let contenedor= document.getElementById("contenedorTabla");
-    contenedor.innerHTML= "<h1> PROBANDO </h1>";
-    let contenido= "";
-    for(let i=1; i<=10; i++){
-        contenido=contenido + "<p>"+"3 x " + i + "=" + (3*i) + "</p>"
+    let contenedor = document.getElementById("contenedorTabla");
+    let numero = document.getElementById("numero").value;
+
+    let contenido = "";
+
+    for (let i = 1; i <= 10; i++) {
+        contenido= contenido + "<tr>" + "<td>" + numero + "5 x " + i + "</td>" + "<td>" + (5 * i) + "</td>" + "</tr>";
     }
-        contenedor.innerHTML = contenido;
+
+    contenedor.innerHTML = contenido;
 }
