d3d8b052cedbb4b75f03187f46f4b45e1f533464
OskarPaul93 <paul_1993oscar@hotmail.com>
2026-04-26 16:46:48 -0500

    Se genera automaticamente la tabla de multiplicar que desee

--- multiplicar.js
+++ multiplicar.js
@@ -1,12 +1,14 @@
 function generarTabla(){
     let contenedor = document.getElementById("contenedorTabla");
-    let numero = document.getElementById("numero").value;
+    let numero= parseInt(document.getElementById("numero").value);
 
     let contenido = "";
 
     for (let i = 1; i <= 10; i++) {
-        contenido= contenido + "<tr>" + "<td>" + numero + "5 x " + i + "</td>" + "<td>" + (5 * i) + "</td>" + "</tr>";
+        contenido= contenido + "<tr>" + "<td>" + numero + " x " + i + "</td>" + "<td>" + (numero * i) + "</td>" + "</tr>";
     }
 
     contenedor.innerHTML = contenido;
 }
+
+
